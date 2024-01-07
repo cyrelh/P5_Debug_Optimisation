a27c030a850f8e86a10eddb244f8b1da24e0570f
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-01-07 12:53:06 +0100

    Navigation dans la modale corrigée

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index-1] || /*Modif pour faire fonctionner la flèche de gauche*/
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index+1] || imagesCollection[0]; /*Modif pour faire fonctionner la flèche de droite*/
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
